e991632b70aaf42c5e3a8fff793a5cca32a2b526
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2017-03-01 13:30:03 +0000

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function fancySchmancy() {
+  return $("p #bacon").addClass("iAmFancy");
+}
+
+function addText("I love Kittens") {
+  return $("p:second").append();
+}
+
+function showDolphin() {
+  $("img[alt='smiling dolphin 2']").show();
+}
+
+function hideImage() {
+  $("img[alt='pie in face']").hide();
+}
+
+function faceIt() {
+  $('#favorite-snacks').fadeIn(2000);
+}
+
+function fadeOut() {
+  $("p:second").fadeOut(0);
+}
+
+function findItByClass() {
+  return $('a').hasClass('flatironLink')
+}
+
+function formValue() {
+  return $("input:last").val();
+}
